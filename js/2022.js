03c4e53aacaa6ff6eed7104fce679bb3a2f2fe35
Dave Tesch <d@vetes.ch>
2022-02-03 10:51:28 -0600

    more theme pieces

---
+++ js/2022.js
@@ -0,0 +1,19 @@
+//dreamspring.js
+
+var dreamspring = {
+
+    hamburger: function(){
+      var $hamburger = $('.nav-trigger a');
+      var $mobileNav = $('ul.header-nav');
+      $hamburger.on('click touchend', function(e){
+        $(this).toggleClass('active');
+        $mobileNav.toggleClass('active');
+        e.preventDefault();
+      })
+    }
+
+}
+
+$(document).ready(function(){
+    dreamspring.hamburger();
+});
