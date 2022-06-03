f67a1780b812a9369be86a8e9a95c49a5ce7ea37
Dave Tesch <d@vetes.ch>
2022-06-03 16:53:52 -0500

    White Label partners page/CSS

---
+++ page-white-label.php
@@ -0,0 +1,122 @@
+<?php include('includes/head-offline.html');?>
+
+<body>
+
+<main id="main" class="white-label">
+  
+    <section class="partner-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 logo logo-partner"><img src="https://www.placecage.com/300/75" alt="Partner Logo Alt Text"></div>
+            <div class="col-sm-6 logo logo-dreamspring"><img src="https://www.placecage.com/c/300/75" alt="DreamSpring"></div>
+        </div>
+    </div>
+    </section>
+
+    <section class="partner-hero">
+        <div class="inner" style="background-image: url(https://www.placecage.com/1280/800);">
+            <h1>Title About Funding Your Small Business</h1>
+        </div>
+        <div class="content text-center">
+            <div class="container">
+            <h2 class="mb-8">Ready to Fund Your Small Business?<br>Apply Today</h2>
+            (button module)
+            </div>
+        </div>
+    </section>
+
+    <section class="partner-intro">
+        <div class="container">
+            <h2 class="text-center">Intro text here</h2>
+            <div class="partner-block logo-left">
+                <div class="inner">
+                    <div class="image">
+                        <img src="https://www.placecage.com/400/150" alt="">
+                    </div>
+                    <div class="content">
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi quo cupiditate facere asperiores tenetur! Similique distinctio alias maiores, nesciunt ipsa at ea iste rerum ratione ipsam accusantium id incidunt quam!
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="partner-block logo-right">
+                <div class="inner">
+                    <div class="image">
+                        <img src="https://www.placecage.com/c/400/150" alt="">
+                    </div>
+                    <div class="content">
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi quo cupiditate facere asperiores tenetur! Similique distinctio alias maiores, nesciunt ipsa at ea iste rerum ratione ipsam accusantium id incidunt quam!
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="pullquote">
+                <div class="inner">
+                    <div class="image">
+                        <img src="https://www.placecage.com/300/300" alt="Alt Text">
+                    </div>
+                    <div class="content">
+                        <span class="quote">"</span>
+                        <!--RichText-->
+                        <p>Our organization believes in our partnership with DreamSpring because, "Sum dolor sit amet. consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+                        <strong>-Name, VP of Org.</strong>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="partner-states">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 content">
+                    <h2>Where do we lend?</h2>
+                    <p>RICH TEXT</p>
+                    BUTTON MODULE
+                </div>
+                <div class="col-lg-7 image">
+                    <div class="map">
+                        <img src="" alt="States offered: AL, AK, AR, BH, TF">
+                        <ul class="sr-only">
+                            <li>AL</li>
+                            <li>AK</li>
+                            <li>AR</li>
+                        </ul>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="partner-products">
+        <div class="container">
+            <h2>What products does DreamSpring offer?</h2>
+            <strong>We have a loan for you.</strong>
+            <p>
+                blah...
+            </p>
+            <div class="product-grid">
+                {% module "module_16542772358751" path="/carbon/module/Product Grid", label="Product Grid" %}
+            </div>
+        </div>
+    </section>
+
+    <section class="stripe-blue partner-questions">
+        <div class="container text-center">
+            <h2>Other questions? We're happy to help!</h2>
+            <p>Blah</p>
+            HUBSPOT FORM
+        </div>
+    </section>
+
+
+
+
+</main>
+
+</body>
+
+<?php include('includes/footer.html');?>
