02abee1f5ca9f8342001bbba9447b0a1c40f4ed7
Dave Tesch <d@vetes.ch>
2022-08-23 09:32:54 -0500

    home page 2022 updates (unfinished)

---
+++ page-home.php
@@ -0,0 +1,191 @@
+<?php include('includes/head-offline.html');?>
+
+<body>
+
+<main id="main" class="home-2022">
+  
+    <section class="home-hero" style="background-image:url('https://www.placecage.com/1280/450');">
+        <div class="hero-copy">
+            <h1>Funding business dreams, vitalizing communities.</h1>
+            <a href="#" class="btn btn-sm btn-primary-primary transition-3d-hover">Button Module</a>
+        </div>
+    </section>
+
+    <section class="home-subhero">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4 item text-center">
+                    <div class="inner">
+                        <img src="https://go.dreamspring.org/hubfs/images/Icons/blue%205.png" alt="Nic Cage">
+                        <span>What type of loans does DreamSpring provide?</span>
+                        <a href="">FIND YOURS →</a>
+                    </div>
+                </div>
+                <div class="col-sm-4 item text-center middle-item">
+                    <div class="inner">
+                        <img src="https://go.dreamspring.org/hubfs/images/Icons/blue%205.png" alt="Nic Cage">
+                        <span>DreamSpring<br>Impact</span>
+                        <a href="">READ CLIENT STORIES →</a>
+                    </div>
+                </div>
+                <div class="col-sm-4 item text-center">
+                    <div class="inner">
+                        <img src="https://go.dreamspring.org/hubfs/images/Icons/blue%205.png" alt="Nic Cage">
+                        <span>Supporting Entrepeneurial Dreams</span>
+                        <a href="">ABOUT DREAMSPRING →</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="home-products stripe-blue text-center">
+        <div class="container">
+            <div class="content">
+                <h2>DreamSpring Business Loans<span></span></h2>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore enim, rem saepe rerum voluptate architecto incidunt porro ullam tenetur eius! Quasi asperiores dolorem cupiditate sunt minima, nostrum adipisci culpa eum!
+                </p>
+            </div>
+            <div class="row">
+                <div class="col-md-3 product">
+                    <img src="https://www.placecage.com/300/300" alt="Product">
+                    <h3>Power Line of Credit</h3>
+                    <a href="#">MORE DETAILS →</a>
+                </div>
+                <div class="col-md-3 product">
+                    <img src="https://www.placecage.com/300/300" alt="Product">
+                    <h3>Small Business Loan</h3>
+                    <a href="#">MORE DETAILS →</a>
+                </div>
+                <div class="col-md-3 product">
+                    <img src="https://www.placecage.com/300/300" alt="Product">
+                    <h3>#TrueGrit Growth Loan</h3>
+                    <a href="#">MORE DETAILS →</a>
+                </div>
+                <div class="col-md-3 product">
+                    <img src="https://www.placecage.com/300/300" alt="Product">
+                    <h3>Fast Forward Project Loan</h3>
+                    <a href="#">MORE DETAILS →</a>
+                </div>
+                <div class="col-md-3 product">
+                    <img src="https://www.placecage.com/300/300" alt="Product">
+                    <h3>SBA Community Advantage</h3>
+                    <a href="#">MORE DETAILS →</a>
+                </div>
+                <div class="col-md-3 product">
+                    <img src="https://www.placecage.com/300/300" alt="Product">
+                    <h3>Commercial Real Estate</h3>
+                    <a href="#">MORE DETAILS →</a>
+                </div>
+                <div class="col-md-3 product">
+                    <img src="https://www.placecage.com/300/300" alt="Product">
+                    <h3>Credit Starter Loan</h3>
+                    <a href="#">MORE DETAILS →</a>
+                </div>
+                <div class="col-md-3 product">
+                    <img src="https://www.placecage.com/300/300" alt="Product">
+                    <h3>Credit Builder Loan</h3>
+                    <a href="#">MORE DETAILS →</a>
+                </div>
+            </div>
+
+            <div class="row text-center">
+                <div class="col-sm-12">
+                    <a href="#" class="btn btn-sm btn-primary-primary transition-3d-hover">Button Module</a>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="home-clients">
+        <div class="container">
+        <div class="row">
+            <div class="col-sm-6 image">
+                <img src="https://www.placecage.com/c/600/450" alt="">
+            </div>
+            <div class="col-sm-6 content">
+                <span class="eyebrow">CLIENT STORIES</span>
+                <h2>DreamSpring Makes an Impact</h2>
+                <div class="content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa numquam, molestias eveniet nostrum nisi adipisci consequatur quis perferendis provident praesentium, nobis et eum eligendi sit ipsa sint labore delectus. Sunt!</p>
+                </div>
+                <a href="#" class="btn btn-sm btn-primary-primary transition-3d-hover">Button Module</a>
+            </div>
+        </div>
+        </div>
+    </section>
+
+    <section class="home-support stripe-blue text-center">
+        <div class="container">
+            <h2>Supporting Entrepeneurial Dreams<span></span></h2>
+            <div class="row">
+                <div class="col-md-6 item">
+                    <div class="inner">
+                        <img src="https://www.placecage.com/600/350" alt="">
+                        <div class="content">
+                        <h3>Annual Report</h3>
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. A delectus assumenda maiores nisi ipsa odio error minus nulla, sunt animi?
+                        </p>
+                        </div>
+                        <a href="#">CHECK IT OUT →</a>
+                    </div>
+                </div>
+                <div class="col-md-6 item">
+                    <div class="inner">
+                        <img src="https://www.placecage.com/c/600/350" alt="">
+                        <div class="content">
+                        <h3>Our Mission &amp; Values</h3>
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. A delectus assumenda maiores nisi ipsa odio error minus nulla, sunt animi?
+                        </p>
+                        </div>
+                        <a href="#">CHECK IT OUT →</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="home-partners">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 content">
+                    <h2>Partnership with DreamSpring Empowers Business</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus magni hic vero, fugit similique sunt, quae velit fuga obcaecati in officiis itaque nam explicabo doloremque totam sed, ducimus laborum aliquam.
+                    </p>
+                    <a href="#" class="btn btn-sm btn-primary-primary transition-3d-hover">Button Module</a>
+                </div>
+                <div class="col-md-6 logos">
+                    <div class="partner-container">
+                        <img src="https://www.placecage.com/160/160" alt="Nic Cage">    
+                        <img src="https://www.placecage.com/160/160" alt="Nic Cage">    
+                        <img src="https://www.placecage.com/160/160" alt="Nic Cage">    
+                        <img src="https://www.placecage.com/160/160" alt="Nic Cage">    
+                        <img src="https://www.placecage.com/160/160" alt="Nic Cage">    
+                        <img src="https://www.placecage.com/160/160" alt="Nic Cage">    
+                        <img src="https://www.placecage.com/160/160" alt="Nic Cage">    
+                        <img src="https://www.placecage.com/160/160" alt="Nic Cage">    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="home-testimonials" style="background-image: url('https://www.placecage.com/1280/450');">
+        <div class="testimonial-copy">
+            <span>"</span>
+            <p>I just always have felt that there is someone rooting for me", Hector says. "It just is heartwarming and puts a spring in my step that I know, if I run into an issue that DreamSpring is a phone call away."</p>
+            <p class="byline">Hector Rodriguez<br>Topline Commercial Services</p>
+            <a href="#" class="btn btn-sm btn-primary-primary transition-3d-hover">Button Module →</a>
+        </div>
+    </section>
+
+</main>
+
+</body>
+
+<?php include('includes/footer.html');?>
